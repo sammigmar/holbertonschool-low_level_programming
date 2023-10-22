6fceb75d8cc1611840646ec936cd6badaf75b945
Samuel Martin <samuelmartin317@gmail.com>
2023-10-22 15:44:08 -0700

    File name 3.0

--- preprocessor/2-main.c
+++ preprocessor/2-main.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 
-int main()
+/**
+ * main - prints the name of the file
+ *
+ * Return: Always 0 (Success)
+ */
+int ain(void)
 {
-  printf("Date : %s\n", __DATE__);
-  printf("Time : %s\n", __TIME__);
-  printf("File : %s\n", __FILE__);
-  printf("Line : %d\n", __LINE__);
-  printf("STDC / ANSI : %d\n", __STDC__);
-
-  return 0;
+	printf("%s\n", __FILE__);
+	return (0);
 }
